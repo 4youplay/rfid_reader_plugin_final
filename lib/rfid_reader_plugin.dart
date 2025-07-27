a6717abac62e527964cb483b1347581f324895bf
Paulo Amaral <pauloamaral93@hotmail.com>
2025-07-27 16:25:28 +0100

    interface added

--- lib/rfid_reader_plugin.dart
+++ lib/rfid_reader_plugin.dart
@@ -1,2 +1,23 @@
-/// Plugin interface em Flutter
-class RfidReaderPlugin {}
+import 'package:flutter/services.dart';
+
+class RfidReaderPlugin {
+  static const MethodChannel _channel = MethodChannel('rfid_reader_plugin');
+
+  static Future<bool> init() async {
+    final result = await _channel.invokeMethod<bool>('init');
+    return result ?? false;
+  }
+
+  static Future<void> startReading() async {
+    await _channel.invokeMethod('startReading');
+  }
+
+  static Future<void> stopReading() async {
+    await _channel.invokeMethod('stopReading');
+  }
+
+  static Future<List<String>> getTags() async {
+    final List<dynamic> tags = await _channel.invokeMethod('getTags');
+    return tags.cast<String>();
+  }
+}
